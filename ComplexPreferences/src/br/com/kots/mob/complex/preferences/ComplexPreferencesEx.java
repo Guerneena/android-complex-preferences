0e94abb746644e11086094be59d7070d69de14b1
kimukou_26 <kimukou.buzz@gmail.com>
2013-09-13 16:00:48 +0900

    update target 18(minsdk 4)

--- ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferencesEx.java
+++ ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferencesEx.java
@@ -1,6 +1,9 @@
 package  br.com.kots.mob.complex.preferences;
 
+import java.util.Set;
+
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
 @SuppressLint("NewApi")
@@ -8,4 +11,9 @@ public class ComplexPreferencesEx {
 	public static void apply(Editor e ){
 		e.apply();
 	}
+	
+	public static Set<String> getStringSet(SharedPreferences preferences,String key, Set<String> values){
+		return preferences.getStringSet(key, values);
+	}
+	
 }
