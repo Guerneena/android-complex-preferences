ccf2b5a326fdb9272e1c9b210ca2a426287a8fb8
kimukou_26 <kimukou.buzz@gmail.com>
2013-09-13 16:15:20 +0900

    deep-copy action

--- ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferencesEx.java
+++ ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferencesEx.java
@@ -1,5 +1,6 @@
-package  br.com.kots.mob.complex.preferences;
+package br.com.kots.mob.complex.preferences;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import android.annotation.SuppressLint;
@@ -8,12 +9,21 @@ import android.content.SharedPreferences.Editor;
 
 @SuppressLint("NewApi")
 public class ComplexPreferencesEx {
-	public static void apply(Editor e ){
+	public static void apply(Editor e) {
 		e.apply();
 	}
-	
-	public static Set<String> getStringSet(SharedPreferences preferences,String key, Set<String> values){
-		return preferences.getStringSet(key, values);
+
+	public static Set<String> getStringSet(SharedPreferences sp, String key,Set<String> values) {
+		// return sp.getStringSet(key, values);
+		
+		// need deep copy
+		//    see http://blog.nagopy.com/2013/02/sharedpreferencesgetstringset.html
+		//
+		Set<String> saved = sp.getStringSet(key, values);
+		Set<String> returnSet = new HashSet<String>(saved.size());
+		for (String str : saved) {
+			returnSet.add(str);
+		}
+		return returnSet;
 	}
-	
 }
